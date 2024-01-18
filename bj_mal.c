5b101ff582f2c4dacf8467e5034abea6f48787bf
Joan Del Pino <jodemo@jviladoms.cat>
2024-01-18 10:56:27 +0100

    Cometar todo el codigo

--- bj_mal.c
+++ bj_mal.c
@@ -6,7 +6,6 @@
 #define VALORES_CARTAS 13
 #define MAX_MANO 10
 
-
 // Representación de las cartas
 char *valores[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K","A"};
 char *palos[] = {"Corazones", "Diamantes", "Picas", "Treboles"};
@@ -23,6 +22,7 @@ int numCartasJugador = 0;
 int sumaJugador = 0;
 char eleccion;
 
+// Función para sumar los valores de una mano y ajustar 'A' según sea necesario
 int sumarMano(struct carta mano[], int numCartas) {
     int suma = 0;
     int numAses = 0;
@@ -44,6 +44,8 @@ int sumarMano(struct carta mano[], int numCartas) {
 
     return suma;
 }
+
+// Función para que el jugador elija el valor de 'A' (1 o 11)
 int elegirValorA() {
     int valor;
     printf("Elige el valor para 'A' (1/11): ");
@@ -56,6 +58,7 @@ int elegirValorA() {
 }
 
 int main() {
+    // Inicializar el mazo de cartas
     for (int i = 0; i < TOTAL_CARTAS; i++) {
         mazo[i].valor = valores[i % VALORES_CARTAS];
         mazo[i].palo = palos[i / VALORES_CARTAS];
@@ -63,6 +66,8 @@ int main() {
         if (mazo[i].valorNumerico > 10) mazo[i].valorNumerico = 10;
         if (i % VALORES_CARTAS == VALORES_CARTAS - 1) mazo[i].valorNumerico = 11;
     }
+
+    // Barajar el mazo
     srand(time(NULL));
     for (int i = 0; i < TOTAL_CARTAS; i++) {
         int j = rand() % TOTAL_CARTAS;
@@ -73,37 +78,40 @@ int main() {
 
     printf("Bienvenido al Blackjack!\n");
 
+    // Repartir las dos primeras cartas al jugador
     manoJugador[numCartasJugador++] = mazo[0];
     manoJugador[numCartasJugador++] = mazo[1];
     sumaJugador = sumarMano(manoJugador, numCartasJugador);
 
+    // Mostrar las cartas iniciales y la suma total
     printf("Tus cartas:\n");
-    printf("  %s de %s\n", manoJugador[0]);
-    printf("  %s de %s\n", manoJugador[1]);
+    printf("  %s de %s\n", manoJugador[0].valor, manoJugador[0].palo);
+    printf("  %s de %s\n", manoJugador[1].valor, manoJugador[1].palo);
     printf("Suma total: %d\n", sumaJugador);
 
+    // Bucle principal del juego
     while (sumaJugador < 21) {
-    printf("¿Quieres otra carta? (s/n): ");
-    scanf(" %c", &eleccion);
+        printf("Quieres otra carta? (s/n): ");
+        scanf(" %c", &eleccion);
 
-    if (eleccion == 's' || eleccion == 'S') {
-        manoJugador[numCartasJugador] = mazo[numCartasJugador + 1];
-        
-        // Verificar si la carta es un 'A' y permitir al jugador elegir su valor
-        if (manoJugador[numCartasJugador].valorNumerico == 11) {
-            manoJugador[numCartasJugador].valorNumerico = elegirValorA();
+        if (eleccion == 's' || eleccion == 'S') {
+            // Extraer una nueva carta y permitir al jugador elegir el valor de 'A'
+            manoJugador[numCartasJugador] = mazo[numCartasJugador + 1];
+            if (manoJugador[numCartasJugador].valorNumerico == 11) {
+                manoJugador[numCartasJugador].valorNumerico = elegirValorA();
+            }
+
+            // Actualizar la suma de la mano y mostrar la nueva carta y la suma total
+            sumaJugador = sumarMano(manoJugador, ++numCartasJugador);
+            printf("Nueva carta:\n");
+            printf("  %s de %s\n", manoJugador[numCartasJugador - 1].valor, manoJugador[numCartasJugador - 1].palo);
+            printf("Suma total: %d\n", sumaJugador);
+        } else {
+            break;
         }
-
-        sumaJugador = sumarMano(manoJugador, ++numCartasJugador);
-
-        printf("Nueva carta:\n");
-        printf("  %s de %s\n", manoJugador[numCartasJugador - 1].valor, manoJugador[numCartasJugador - 1].palo);
-        printf("Suma total: %d\n", sumaJugador);
-    } else {
-        break;
-    }
     }
 
+    // Determinar el resultado del juego y mostrar el mensaje correspondiente
     if (sumaJugador == 21) {
         printf("¡Blackjack! Has ganado.\n");
     } else if (sumaJugador > 21) {
