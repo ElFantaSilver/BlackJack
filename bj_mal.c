809db79b6197d7c84fd6618cc66de5b13781956f
Joan Del Pino <jodemo@jviladoms.cat>
2024-01-18 10:51:54 +0100

    Añadir el cambio de valor de A

--- bj_mal.c
+++ bj_mal.c
@@ -6,9 +6,10 @@
 #define VALORES_CARTAS 13
 #define MAX_MANO 10
 
+
 // Representación de las cartas
-char *valores[] = {"A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-char *palos[] = {"Corazones", "Diamantes", "Picas", "Tréboles"};
+char *valores[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K","A"};
+char *palos[] = {"Corazones", "Diamantes", "Picas", "Treboles"};
 
 // Estructura para una carta
 struct carta {
@@ -16,7 +17,6 @@ struct carta {
     char *palo;
     int valorNumerico;
 };
-
 struct carta mazo[TOTAL_CARTAS];
 struct carta manoJugador[MAX_MANO];
 int numCartasJugador = 0;
@@ -25,20 +25,41 @@ char eleccion;
 
 int sumarMano(struct carta mano[], int numCartas) {
     int suma = 0;
+    int numAses = 0;
+
     for (int i = 0; i < numCartas; i++) {
         suma += mano[i].valorNumerico;
+
+        // Contar la cantidad de 'A' en la mano
+        if (mano[i].valorNumerico == 11) {
+            numAses++;
+        }
     }
+
+    // Ajustar el valor de 'A' si la suma supera 21
+    while (suma > 21 && numAses > 0) {
+        suma -= 10;
+        numAses--;
+    }
+
     return suma;
+}
+int elegirValorA() {
+    int valor;
+    printf("Elige el valor para 'A' (1/11): ");
+    scanf("%d", &valor);
+    while (valor != 1 && valor != 11) {
+        printf("Por favor, elige 1 o 11 para 'A': ");
+        scanf("%d", &valor);
+    }
+    return valor;
 }
 
 int main() {
-    inicializarMazo();
-    barajarMazo();
-
     for (int i = 0; i < TOTAL_CARTAS; i++) {
         mazo[i].valor = valores[i % VALORES_CARTAS];
         mazo[i].palo = palos[i / VALORES_CARTAS];
-        mazo[i].valorNumerico = (i % VALORES_CARTAS) + 1;
+        mazo[i].valorNumerico = (i % VALORES_CARTAS) + 2;
         if (mazo[i].valorNumerico > 10) mazo[i].valorNumerico = 10;
         if (i % VALORES_CARTAS == VALORES_CARTAS - 1) mazo[i].valorNumerico = 11;
     }
@@ -62,19 +83,25 @@ int main() {
     printf("Suma total: %d\n", sumaJugador);
 
     while (sumaJugador < 21) {
-        printf("¿Quieres otra carta? (s/n): ");
-        scanf(" %c", &eleccion);
+    printf("¿Quieres otra carta? (s/n): ");
+    scanf(" %c", &eleccion);
 
-        if (eleccion == 's' || eleccion == 'S') {
-            manoJugador[numCartasJugador] = mazo[numCartasJugador + 1];
-            sumaJugador = sumarMano(manoJugador, ++numCartasJugador);
-
-            printf("Nueva carta:\n");
-            printf("  %s de %s\n", manoJugador[numCartasJugador - 1]);
-            printf("Suma total: %d\n", sumaJugador);
-        } else {
-            break;
+    if (eleccion == 's' || eleccion == 'S') {
+        manoJugador[numCartasJugador] = mazo[numCartasJugador + 1];
+        
+        // Verificar si la carta es un 'A' y permitir al jugador elegir su valor
+        if (manoJugador[numCartasJugador].valorNumerico == 11) {
+            manoJugador[numCartasJugador].valorNumerico = elegirValorA();
         }
+
+        sumaJugador = sumarMano(manoJugador, ++numCartasJugador);
+
+        printf("Nueva carta:\n");
+        printf("  %s de %s\n", manoJugador[numCartasJugador - 1].valor, manoJugador[numCartasJugador - 1].palo);
+        printf("Suma total: %d\n", sumaJugador);
+    } else {
+        break;
+    }
     }
 
     if (sumaJugador == 21) {
